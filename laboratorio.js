c93e92d24ccad79797170c6182aa01aa3731d4ae
Enrique Muñoz-Torrero <152556786+Enrique2518@users.noreply.github.com>
2025-11-11 16:32:35 +0000

    Fisrt Push

---
+++ laboratorio.js
@@ -0,0 +1,40 @@
+//Ex.3
+console.log("Hola Mundo")
+
+//Ex.4
+let palabra1 = "Hola";
+const palabra2 = "Adiós"
+console.log(palabra1 + "es para saludar y " + palabra2 + " es para despedirse")
+let nombre = "Enrique"
+const edad = 25
+console.log(`Hola, me llamo ${nombre} y tengo ${edad} años.`)
+
+//Ex.5
+var nom = "Toni"
+var edat = 8
+var estudiante = true
+var vacio = null
+console.log(nom + " " + edat + " " + estudiante + " " + vacio + " ")
+
+//Ex.6
+var num1 = 8;
+var num2 = 4;
+var suma = num1 + num2
+console.log("Suma:" +suma)
+var resta = num1 - num2
+console.log("Resta: " + resta)
+var multiplicacion = num1 * num2
+console.log("Multiplicación: " + multiplicacion)
+var division = num1 / num2
+console.log("División: " + division)
+var resto = num1 % num2
+console.log("Resto: " + resto)
+
+var igualAbs = num1 == num2
+console.log("Igualdad abstracta : " + igualAbs)
+var igualEst = num1 === num2
+console.log("Igualdad estricta : " + igualEst)
+var desigualdad = num1 != num2
+console.log("Igualdad estricta : " + desigualdad)
+var desigualdadEstricta = num1 !== num2
+console.log("Igualdad estricta : " + desigualdadEstricta)
